8688603a9209d160632358c5a558ed06bb096d17
Luiza Fernandes <luhqfernandes@gmail.com>
2021-02-26 16:30:00 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+//componentes fixos o ChallengesContext.Provider da acesso aos dados do contexto
+import '../styles/global.css'
+
+function MyApp({ Component, pageProps }) {
+ 
+  return(
+      <Component {...pageProps} />
+  ) 
+}
+
+export default MyApp
